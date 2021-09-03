ddb17c694a2d6d94571dc32c271940c32d4d181c
Tai Nguyen <taisnguyen123@yahoo.com>
2021-09-03 05:44:28 -0400

    added sketch-figure.js

---
+++ root/js/models/sketch-figure.js
@@ -0,0 +1,23 @@
+/**
+ * AUTHOR:	TAI . SANH . NGUYEN
+ *
+ * [E] 
+ *  [T] 
+ *   [C] 
+ *    [H] 
+ *
+ * defines the SketchFigure class
+ */
+
+
+
+
+/** 
+ * represents a SketchFigure instance  
+ */
+export class SketchFigure {
+
+    constructor() {
+
+    }
+}
