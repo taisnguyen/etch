56459cad316eb14b14eaf39dc98c06f9dbacc28e
Tai Nguyen <taisnguyen123@yahoo.com>
2021-09-09 05:18:25 -0400

    added image assets

--- root/js/models/text-editor-canvas.js
+++ root/js/models/text-editor-canvas.js
@@ -18,30 +18,55 @@ import { SketchFigure } from "./sketch-figure.js";
 /** 
  * represents a TextEditorCanvas instance  
  */
- export class TextEditorCanvas {
+export class TextEditorCanvas {
 
-    /**
-     * @param {Object} data an object containing key/value pairs of data
-     * {
-     *    textEditorDOMElement : reference to the DOM element representing the TextEditorCanvas
-     * }
-     */
+   /**
+    * @param {Object} data an object containing key/value pairs of data
+    * {
+    *    textEditorCanvasDOMElement : reference to the DOM element representing the TextEditorCanvas
+    *    textEditorDOMElement       : reference to the DOM element of the parent TextEditor
+    * }
+    */
 
-     constructor(data) {
-        this._data = data || {};
-        this._DOMElement = null;
+   constructor(data) {
+      this._data = data || {};
+      this._DOMElement = null;
 
-        this._initialize();
-    }
+      this._initialize();
+   }
 
-    _initializeDOMElement() {
-       
-    }
+   _initializeDOMElement() {
+      const textEditorCanvasWrapper = document.createElement("div");
+      textEditorCanvasWrapper.innerHTML = '<canvas class="text-editor-canvas"></canvas>';
+      textEditorCanvasWrapper.classList.add("text-editor-canvas-wrapper");
 
-    _initialize() {
+      this._DOMElement = textEditorCanvasWrapper;
+   }
 
-    }
+   _initialize() {
+      // initialize DOMElement and replace TextEditorCanvas shell div
+      this._initializeDOMElement();
+      this.data["textEditorCanvasDOMElement"].replaceWith(this.DOMElement);
+   }
 
-    
 
- }
+
+
+   get data() {
+      return this._data;
+   }
+
+   set data(data) {
+      this._data = data;
+   }
+
+   get DOMElement() {
+      return this._DOMElement;
+   }
+
+   set DOMElement(DOMElement) {
+      this._DOMElement = DOMElement;
+   }
+
+
+}
