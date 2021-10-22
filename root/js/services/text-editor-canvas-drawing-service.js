f8e1f5683f037cd866081f9c1913a5eb231bcf19
Tai Nguyen <taisnguyen123@yahoo.com>
2021-10-22 03:47:53 -0400

    implementing TextEditorCanvasDrawingService singleton

---
+++ root/js/services/text-editor-canvas-drawing-service.js
@@ -0,0 +1,13 @@
+/**
+ * AUTHOR:	TAI . SANH . NGUYEN
+ *
+ * [E] 
+ *  [T] 
+ *   [C] 
+ *    [H] 
+ *
+ * an interface for dependents of the drawing functionality of a TextEditorCanvas instance
+ * 
+ * singleton (returns same instance of this class if provided TextEditorCanvas is the same)
+ */
+
