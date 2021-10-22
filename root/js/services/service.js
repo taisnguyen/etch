4647c4512afcad0cb2f4368eee4f10c083156c70
Tai Nguyen <taisnguyen123@yahoo.com>
2021-10-22 04:56:04 -0400

    added service interface for all services to derive from

---
+++ root/js/services/service.js
@@ -0,0 +1,25 @@
+/**
+ * AUTHOR:	TAI . SANH . NGUYEN
+ *
+ * [E] 
+ *  [T] 
+ *   [C] 
+ *    [H] 
+ *
+ * defines the service interface
+ * 
+ * all service classes must derive from this class
+ */
+
+
+
+
+export class Service {
+
+    /**
+     * @param {string} type type of Service
+     */
+    constructor(type) {
+        this.type = type;
+    }
+} 
