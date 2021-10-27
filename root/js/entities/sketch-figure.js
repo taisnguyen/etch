26378de73389fc65f3001ad388aaca2f6a47a782
Tai Nguyen <taisnguyen123@yahoo.com>
2021-10-27 12:52:40 -0400

    updated draw method to interface with drawing service

--- root/js/entities/sketch-figure.js
+++ root/js/entities/sketch-figure.js
@@ -11,6 +11,7 @@
 
 
 import { SketchFigureBoundingBox } from "./sketch-figure-bounding-box.js";
+import { TextEditorCanvasDrawingServiceFactory } from "../services/text-editor-canvas-drawing-service-factory.js";
 
 
 
@@ -33,7 +34,6 @@ export class SketchFigure {
         this.selected = false;
         this.color = color;
         
-
         this._initialize();
     }
 
@@ -51,33 +51,40 @@ export class SketchFigure {
     _initialize() {
         this._initializeBoundingBox();
     }
- 
-    draw(canvasContext) {
-        canvasContext.save();
-        canvasContext.strokeStyle = this.color;
-        canvasContext.beginPath();
-        canvasContext.moveTo(this._sketchPoints[0].x, this._sketchPoints[0].y);
-        
-        // not enough points to compute bezier curve, draw a normal line for each SketchPoint
-        if (this._sketchPoints.length < 6) {
-            for (let i = 0; i < this._sketchPoints.length; i++) {
-                canvasContext.lineTo(this._sketchPoints[i].x, this._sketchPoints[i].y);
+
+    draw(textEditorCanvas, saveToState = false) {
+        const drawingService = TextEditorCanvasDrawingServiceFactory.getService(textEditorCanvas);
+        const drawingFunction = (canvasContext) => {
+            canvasContext.save();
+            canvasContext.strokeStyle = this.color;
+            canvasContext.beginPath();
+            canvasContext.moveTo(this._sketchPoints[0].x, this._sketchPoints[0].y);
+            
+            // not enough points to compute bezier curve, draw a normal line for each SketchPoint
+            if (this._sketchPoints.length < 6) {
+                for (let i = 0; i < this._sketchPoints.length; i++) {
+                    canvasContext.lineTo(this._sketchPoints[i].x, this._sketchPoints[i].y);
+                }
+                canvasContext.stroke();
+                canvasContext.restore();
+                return;
+            }
+
+            // using bezier curves, connect the points in the SketchFigure
+            for (let i = 1; i < this._sketchPoints.length - 2; i++) {
+                const averageX = (this._sketchPoints[i].x + this._sketchPoints[i + 1].x) / 2;
+                const averageY = (this._sketchPoints[i].y + this._sketchPoints[i + 1].y) / 2;
+                canvasContext.quadraticCurveTo(this._sketchPoints[i].x, this._sketchPoints[i].y, averageX, averageY);
             }
             canvasContext.stroke();
             canvasContext.restore();
-            return;
-        }
+        };
 
-        // using bezier curves, connect the points in the SketchFigure
-        for (let i = 1; i < this._sketchPoints.length - 2; i++) {
-            const averageX = (this._sketchPoints[i].x + this._sketchPoints[i + 1].x) / 2;
-            const averageY = (this._sketchPoints[i].y + this._sketchPoints[i + 1].y) / 2;
-            canvasContext.quadraticCurveTo(this._sketchPoints[i].x, this._sketchPoints[i].y, averageX, averageY);
-        }
-        canvasContext.stroke();
-        canvasContext.restore();
+        if (saveToState)
+            drawingService.drawSaveToState(drawingFunction);
+        else
+            drawingService.drawNoSaveToState(drawingFunction);
     }
-
 
     get boundingBox() {
         return this._boundingBox;
