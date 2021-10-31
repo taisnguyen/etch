f760f5d2a6a9710fc6608ba6b4199a88f73ead33
Tai Nguyen <taisnguyen123@yahoo.com>
2021-10-31 05:26:16 -0400

    split file into two

--- root/js/services/text-editor-canvas-drawing-service-factory.js
+++ root/js/services/text-editor-canvas-drawing-service-factory.js
@@ -14,54 +14,10 @@
  */
 
 
-import { Service } from "./service.js";
+import { TextEditorCanvasDrawingService } from "./text-editor-canvas-drawing-service.js";
 
 
 
-
-class TextEditorCanvasDrawingService extends Service {
-
-    constructor(textEditorCanvas) {
-        // calls Service base class's constructor
-        super("TextEditorCanvasDrawingService");
-
-        this._textEditorCanvas = textEditorCanvas;
-        this._canvasContext = textEditorCanvas.canvasContext;
-    }
-
-    drawAllDrawableObjects(drawableObjects) {
-        const arraysOfDrawableObjects = Object.values(drawableObjects);
-        for (const arrayOfDrawableObjects of arraysOfDrawableObjects) {
-            for (const drawableObject of arrayOfDrawableObjects) {
-                drawableObject.draw(this.canvasContext);
-            }
-        }
-    }
-    
-    refresh() {
-        this.canvasContext.clearRect(0, 0, this.canvasContext.canvas.width, this.canvasContext.canvas.height);
-        this.drawAllDrawableObjects(this.textEditorCanvas.drawableObjects);
-    }
-
-    /**
-     * @param {function(canvasContext : CanvasRenderingContext2D)} drawingFunction
-     */
-    draw(drawingFunction) {
-        this.canvasContext.clearRect(0, 0, this.canvasContext.canvas.width, this.canvasContext.canvas.height);
-        drawingFunction(this.canvasContext);
-        this.drawAllDrawableObjects(this.textEditorCanvas.drawableObjects);
-    }
-
-    get textEditorCanvas() {
-        return this._textEditorCanvas;
-    }
-
-    get canvasContext() {
-        return this._canvasContext;
-    }
-
-    
-}
 
 export class TextEditorCanvasDrawingServiceFactory {
 
