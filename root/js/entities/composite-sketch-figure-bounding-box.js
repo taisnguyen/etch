d5383940c7fc82841a927e19e94c61caaea4447f
Tai Nguyen <taisnguyen123@yahoo.com>
2021-10-31 05:09:32 -0400

    implementing CompositeSketchFigureBoundingBox class

---
+++ root/js/entities/composite-sketch-figure-bounding-box.js
@@ -0,0 +1,24 @@
+/**
+ * AUTHOR:	TAI . SANH . NGUYEN
+ *
+ * [E] 
+ *  [T] 
+ *   [C] 
+ *    [H] 
+ *
+ * defines the CompositeSketchFigureBoundingBox class
+ */
+
+
+import { SketchFigureBoundingBox } from "./sketch-figure-bounding-box.js";
+
+
+
+
+export class CompositeSketchFigureBoundingBox {
+
+    constructor() {
+        
+    }
+} 
+
